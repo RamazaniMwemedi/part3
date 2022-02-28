e3a7aab82e482b9faa06f7c1024451d183c7b4e7
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-02-28 16:06:06 +0300

    'Deploy'

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
+// console.log(Note);
+const app = express()
+const port = process.env.PORT
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -9,48 +18,41 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-
-
-const theNote = []
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    // console.log(note)
-    theNote.push(note)
-    console.log(theNote);
-  })
-  mongoose.connection.close()
+const note = new Note({
+  content: "Ramazani",
+  date: new Date,
+  import: true
 })
+console.log("Started");
+// note.save().then(res=>{
+//   console.log("Data sent to the DataBase ");
+//   // mongoose.connection.close()
+// })
+
+console.log("Hello there", process.env.NAME);
+
+// app.get('/', (req, res)=>{
+//   res.send("Home page")
+// })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+
+//   noteSchema.set('toJSON',{
+//   transform:(document, returnOdject)=>{
+//     returnOdject.id= returnOdject._id.toString()
+//     delete returnOdject.id
+//     delete returnOdject.__v
+//   }
+// })
+
+  Note.find({}).then( note=>{
+    res.json(note)
+  })
+  console.log("API asked");
 })
 
 app.get('/notes', (req, res)=>{
@@ -58,47 +60,16 @@ app.get('/notes', (req, res)=>{
 })
 
 app.get('/info', (req, res)=>{
-  const theDate = new Date()
-  res.send(`<h1>Phone book has info for ${persons.length} people </h1> <br/><h3>${theDate}</h3>` )
-})
-
-app.get('/api/persons/:id', (req, res)=>{
-  const id = Number(req.params.id)
-  const person = persons.find(person=> person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.delete('/api/persons/:id', (req, res)=>{
-  const id = Number(req.params.id)
-  persons = persons.filter(person=> person.id !== id)
-  res.status(204).end()
-})
-
-app.post('/api/persons', (req, res)=>{
-  let body = req.body
-  const name = body.name
-  const important = body.important || false
-  const id = Math.floor((Math.random()* 9^9999) + 1)
-
-  const names = persons.map(person => person.name )
-  const x = names.filter(useName => useName)
-  
-  if (x == name.toUpperCase()) {
-    console.log(`This name:${name}, is found `);
-    res.status(406).send('Something')
-  } else{
-    console.log(`${name} is not found`);
-    res.status(200).send('also something')
-  }
+  Note.find({}).then( note=>{
+   res.send(`<h1>Phone book has info for ${note.length} people</h1> <i>${new Date()}</i>`)
+  })
+  console.log("Info point asked");
 })
 
 const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send(`<h2>Hello😎</h2> <p>You are lost here, there is no such kindd of thing here dude 😁😁😁</p>`)
   next()
+  console.log("The unknown end point", request);
 }
 
 app.use(unknownEndpoint)
