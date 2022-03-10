1b3b3ffc0a05ca29e1aa91dda16aa64ba25d183c
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-03-10 16:39:40 +0300

    Finished exercise 3.22

--- mongo.js
+++ mongo.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
+  console.log("Please provide the password as an argument: node mongo.js <password>")
   process.exit(1)
 }
 const url = `mongodb+srv://RamazaniMwemedi:${password}cluster0.t3hxb.mongodb.net/noteApp?retryWrites=true&w=majority`
@@ -19,7 +19,7 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-const Note = new mongoose.model('Note', noteSchema)
+const Note = new mongoose.model("Note", noteSchema)
 
 if (process.argv.length === 3 ){
   Note.find({}).then(result => {
